265156066bf38846e47103550949ef0410754bf2
Quinnton Perri <quinnclimbs@gmail.com>
2024-06-14 13:20:48 -0500

    Make Clock Work

--- app/Controllers/ClockController.js
+++ app/Controllers/ClockController.js
@@ -1,8 +1,9 @@
 
 function _DrawClock() {
   var today = new Date();
-  var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  var time = today.getHours() + ":" + today.getMinutes();
+  var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear() ;
+  var isPM = today.getHours() > 12;
+  var time = (isPM ? today.getHours() - 12 : today.getHours()) + ":" + (today.getMinutes().toString().length == 1 ? "0" + today.getMinutes().toString() : today.getMinutes().toString()) + (isPM ? " P.M." : " A.M." );
   document.getElementById('clock').innerText = time
   document.getElementById('date').innerText = date
 
