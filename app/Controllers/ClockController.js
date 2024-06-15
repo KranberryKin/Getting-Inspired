fd558bd67c06d8c65dfa147638cb2880f381408f
Quinnton Perri <quinnclimbs@gmail.com>
2024-06-15 00:13:50 -0500

    Update clock for midnight

--- app/Controllers/ClockController.js
+++ app/Controllers/ClockController.js
@@ -3,7 +3,8 @@ function _DrawClock() {
   var today = new Date();
   var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear() ;
   var isPM = today.getHours() > 12;
-  var time = (isPM ? today.getHours() - 12 : today.getHours()) + ":" + (today.getMinutes().toString().length == 1 ? "0" + today.getMinutes().toString() : today.getMinutes().toString()) + (isPM ? " P.M." : " A.M." );
+  var isMidnight = today.getHours() === 0;
+  var time = (isPM ? today.getHours() - 12 : isMidnight ? today.getHours() + 12 : today.getHours()) + ":" + (today.getMinutes().toString().length == 1 ? "0" + today.getMinutes().toString() : today.getMinutes().toString()) + (isPM ? " P.M." : " A.M." );
   document.getElementById('clock').innerText = time
   document.getElementById('date').innerText = date
 
